c785019e037f3ac7340e98a5913ea9db6e6d21e6
Joyce Liang <qilian@microsoft.com>
2022-07-15 11:55:55 -0700

    add ErrorTemplate sample

---
+++ WpfAppErrorTemplate/WpfAppErrorTemplate/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.PlatformUI;
+
+using System.ComponentModel;
+
+namespace WpfAppErrorTemplate
+{
+
+    internal class MainWindowViewModel : ObservableObject, IDataErrorInfo
+    {
+        public MainWindowViewModel()
+        {
+            this.Email = "input your email";
+        }
+
+        private string eamil;
+        public string Email 
+        { 
+            get => this.eamil; 
+            set => this.SetProperty(ref this.eamil, value); 
+        }
+
+        private string errorMessage;
+        public string ErrorMessage
+        {
+            get => this.errorMessage;
+            set => this.SetProperty(ref this.errorMessage, value);
+        }
+
+        #region IDataErrorInfo
+
+        public string Error { get;}
+
+        public string this[string columnName] 
+        { 
+            get
+            {
+                string result = string.Empty;
+
+                if (columnName == nameof(Email))
+                {
+                    if (string.IsNullOrEmpty(this.Email))
+                    {
+                        result = "Email is required";
+                        this.ErrorMessage = result;
+                    }
+                    else
+                    {
+                        this.ErrorMessage = string.Empty;
+                    }
+                }
+
+                return result;
+            }
+        }
+        #endregion
+
+    }
+}
