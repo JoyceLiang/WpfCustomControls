222cedc421ae78b8e7755c597fbe5d84416e879b
Joyce Liang <qilian@microsoft.com>
2022-09-07 16:43:25 -0700

    Wrap control by trigger sample

---
+++ WrapControlByTrigger/WrapControlByTrigger/WidthLimitConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WrapControlByTrigger
+{
+    public class WidthLimitConverter : IValueConverter
+    {
+        public double WidthLimit { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double width)
+            {
+                return width <= this.WidthLimit;
+            }
+
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
